d987c56cce5fbd92215284486fbf1de8ca6d9283
Simon Storf <simonstorf@gmail.com>
2024-07-14 14:22:32 +0200

    auth fixes

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://nice-squid-27.clerk.accounts.dev/",
+        domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
         applicationID: "convex",
       },
     ]
